e55cd73325cd0353d5b9b0956bdf315c2df19d2a
phaneendra <phaneendraandukuri@gmail.com>
2020-04-22 21:35:54 +0530

    modified separate_nums_in_range function
    removed unnecesary 'if's from the function

--- separate_nums_in_range.c
+++ separate_nums_in_range.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 #include "separate_nums_in_range.h"
 
-// Numbers_in_range *copy_number_list(int *source[], int *sizes)
-// {
-// 	Numbers_in_range *final = malloc(sizeof(Numbers_in_range) * 3);
-// 	for (int i = 0; i < 3; i++)
-// 	{
-// 		final[i].length = sizes[i];
-// 		final[i].numbers = malloc(sizeof(int) * sizes[i]);
-// 		for (int j = 0; j < final[i].length; j++)
-// 		{
-// 			final[i].numbers[j] = source[i][j];
-// 		}
-// 	}
-// 	return final;
-// }
-
 void display_numbers_in_range(Numbers_in_range *numbersList)
 {
 	printf("[\n");
@@ -39,21 +24,9 @@ Numbers_in_range *separate_nums_in_range(int *number_list, int start, int end, i
 
 	for (int i = 0; i < length; i++)
 	{
-		if (number_list[i] < start)
-		{
-			separated_list[0][sizes[0]] = number_list[i];
-			sizes[0]++;
-		}
-		if (number_list[i] > end)
-		{
-			separated_list[2][sizes[2]] = number_list[i];
-			sizes[2]++;
-		}
-		if (number_list[i] >= start && number_list[i] <= end)
-		{
-			separated_list[1][sizes[1]] = number_list[i];
-			sizes[1]++;
-		}
+		int num = number_list[i] < start ? 0 : (number_list[i] > end) ? 2 : 1;
+		separated_list[num][sizes[num]] = number_list[i];
+		sizes[num]++;
 	}
 
 	Numbers_in_range *final = malloc(sizeof(Numbers_in_range) * 3);
